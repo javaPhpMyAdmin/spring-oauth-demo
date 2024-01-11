ce13bd359db7f6111e617509092fe143bbf5d037
javaPhpMyAdmin <chelobat16411@gmail.com>
2024-01-11 00:16:34 -0300

    project started

---
+++ src/main/java/com/oauth2test/AuthenticatingApplication.java
@@ -0,0 +1,13 @@
+package com.oauth2test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticatingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticatingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oauth2test/AuthenticatingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oauth2test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticatingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
